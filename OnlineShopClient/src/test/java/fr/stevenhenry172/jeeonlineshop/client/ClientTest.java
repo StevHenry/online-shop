988fadd2efea6bf60802e6f8adb8a79f8dfcf916
Steven HENRY <stevenhenry83270@gmail.com>
2024-01-13 00:03:58 +0100

    Added some tests

--- OnlineShopClient/src/test/java/fr/stevenhenry172/jeeonlineshop/client/ClientTest.java
+++ OnlineShopClient/src/test/java/fr/stevenhenry172/jeeonlineshop/client/ClientTest.java
@@ -1,16 +1,22 @@
 package fr.stevenhenry172.jeeonlineshop.client;
 
 import fr.stevenhenry172.jeeonlineshop.bean.manager.AccountManagerRemote;
+import fr.stevenhenry172.jeeonlineshop.bean.manager.CartManagerRemote;
+import fr.stevenhenry172.jeeonlineshop.bean.manager.OrderManagerRemote;
+import fr.stevenhenry172.jeeonlineshop.bean.manager.ProductManagerRemote;
+import fr.stevenhenry172.jeeonlineshop.entities.Article;
+import fr.stevenhenry172.jeeonlineshop.entities.Product;
 import fr.stevenhenry172.jeeonlineshop.entities.UserAccount;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import java.rmi.RemoteException;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Optional;
 
 public class ClientTest {
@@ -30,7 +36,7 @@ public class ClientTest {
         try {
             context = new InitialContext(jndiProperties);
             AccountManagerRemote accountManager = (AccountManagerRemote)
-                    context.lookup("ejb:JEE-OnlineShop/OnlineShop-EJB/AccountManager!fr.stevenhenry172.jeeonlineshop.bean.manager.AccountManagerRemote");
+                    context.lookup("ejb:JEE-OnlineShop/OnlineShop-EJB/AccountManagerRemote!fr.stevenhenry172.jeeonlineshop.bean.manager.AccountManagerRemote");
             Optional<UserAccount> userOpt = accountManager.createAccount("Steven", "HENRY", "shenry", "mySuperP@ssword");
             if (userOpt.isEmpty()) {
                 user = accountManager.attemptLogin("shenry", "mySuperP@ssword")
@@ -44,13 +50,36 @@ public class ClientTest {
         }
     }
 
-    @Test
-    public void addArticleTest(){
-
-    }
-
     @AfterAll
     public static void end() throws NamingException {
         context.close();
+    }
+
+    @Test
+    public void addRemoveArticleTest() throws NamingException {
+        ProductManagerRemote pm = (ProductManagerRemote)
+                context.lookup("ejb:JEE-OnlineShop/OnlineShop-EJB/ProductManagerRemote!fr.stevenhenry172.jeeonlineshop.bean.manager.ProductManagerRemote");
+        List<Product> productList = pm.getAllProducts(user);
+        if (!productList.isEmpty()) {
+            CartManagerRemote cartManager = (CartManagerRemote)
+                    context.lookup("ejb:JEE-OnlineShop/OnlineShop-EJB/CartManagerRemote!fr.stevenhenry172.jeeonlineshop.bean.manager.CartManagerRemote");
+            Article<Product> article = new Article<>();
+            article.setProduct(productList.get(0));
+            article.setQuantity(2);
+            if (productList.get(0).getStock() > 0) {
+                Assertions.assertTrue(cartManager.addArticle(user, article));
+                Assertions.assertTrue(cartManager.removeArticle(user, article));
+            } else {
+                Assertions.assertFalse(cartManager.addArticle(user, article));
+                Assertions.assertFalse(cartManager.removeArticle(user, article));
+            }
+        }
+    }
+
+    @Test
+    public void convertToOrderTest() throws NamingException {
+        OrderManagerRemote orderManager = (OrderManagerRemote)
+                context.lookup("ejb:JEE-OnlineShop/OnlineShop-EJB/OrderManagerRemote!fr.stevenhenry172.jeeonlineshop.bean.manager.OrderManagerRemote");
+        orderManager.validateCart(user);
     }
 }
